d7eceb8209c0b92767d3fb3046133ad29bbaf423
developergu <developergu@126.com>
2019-03-19 06:57:14 +0800

    touch event test

---
+++ app/src/main/java/com/example/dispatcheventtest/BLinearLayout.java
@@ -0,0 +1,37 @@
+package com.example.dispatcheventtest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+public class BLinearLayout extends LinearLayout {
+  private static final String TAG = "B linearlayout";
+
+  public BLinearLayout(Context context) {
+    super(context);
+  }
+
+  public BLinearLayout(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  public boolean dispatchTouchEvent(MotionEvent ev) {
+    Log.e(TAG, "dispatchTouchEvent: " + ev.getAction());
+    return super.dispatchTouchEvent(ev);
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent ev) {
+    Log.e(TAG, "onInterceptTouchEvent: " + ev.getAction());
+    return false;
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    Log.e(TAG, "onTouchEvent: " + event.getAction());
+    return false;
+  }
+}
---
+++ app/src/main/java/com/example/dispatcheventtest/CLinearLayout.java
@@ -0,0 +1,37 @@
+package com.example.dispatcheventtest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+public class CLinearLayout extends LinearLayout {
+  private static final String TAG = "C linearlayout";
+
+  public CLinearLayout(Context context) {
+    super(context);
+  }
+
+  public CLinearLayout(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  public boolean dispatchTouchEvent(MotionEvent ev) {
+    Log.e(TAG, "dispatchTouchEvent: " + ev.getAction());
+    return super.dispatchTouchEvent(ev);
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent ev) {
+    Log.e(TAG, "onInterceptTouchEvent: " + ev.getAction());
+    return true;
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    Log.e(TAG, "onTouchEvent: " + event.getAction());
+    return false;
+  }
+}
